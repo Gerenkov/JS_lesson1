1a0b2339444a367f14aa5f2a55437a0047d1ba6e
Gerenkov Vitalii <Gerenkov3830br@mail.ru>
2023-03-18 17:13:25 +0800

    nev save

---
+++ Введение в JS/js/script.js
@@ -0,0 +1,23 @@
+// Задание 1
+/* let result = prompt('Введите значение', '')
+if (result === null) {
+	alert('Вы отказались от ввода')
+} else if (isNaN(result % 2)) {
+	alert('Упс, кажется вы ошиблись')
+} else if (result % 2 === 0) {
+	alert('Число четное')
+} else {
+	alert('число нечетное')
+} */
+
+// Задание 2
+/* let x;
+if (typeof x === 'number') {
+	console.log('число')
+} else if (typeof x === 'string') {
+	console.log('строка')
+} else if (typeof x === 'boolean') {
+	console.log('логический тип')
+} else if (typeof x === 'undefined') {
+	console.log('Тип x не определён')
+} */
